0e0b4aa88db8c8c78dd51f2444c62ec59f6dd132
TheLeoP <eugenio2305@hotmail.com>
2024-01-03 11:14:03 -0500

    explore: minimal_init.lua

--- tests/minimal_init.lua
+++ tests/minimal_init.lua
@@ -1,9 +1,24 @@
-local plenary_dir = os.getenv "PLENARY_DIR" or "/tmp/plenary.nvim"
-local is_not_a_directory = vim.fn.isdirectory(plenary_dir) == 0
-if is_not_a_directory then vim.fn.system { "git", "clone", "https://github.com/nvim-lua/plenary.nvim", plenary_dir } end
+---@param name string
+local function bootstrap(name)
+  local dir = ("/tmp/" .. name)
+  local is_not_a_directory = vim.fn.isdirectory(dir) == 0
+  if is_not_a_directory then vim.fn.system { "git", "clone", "https://github.com/" .. name, dir } end
+  vim.opt.rtp:append(dir)
+end
 
 vim.opt.rtp:append "."
-vim.opt.rtp:append(plenary_dir)
+bootstrap "nvim-lua/plenary.nvim"
+bootstrap "williamboman/mason.nvim"
 
 vim.cmd "runtime plugin/plenary.vim"
-require "plenary.busted"
+-- require "plenary.busted"
+
+local Package = require "mason-core.package"
+local registry = require "mason-registry"
+
+require("mason-registry").refresh()
+local package_name, version = Package.Parse "powershell-editor-services"
+local pkg = registry.get_package(package_name)
+return pkg:install {
+  version = version,
+}
