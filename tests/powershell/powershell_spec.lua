4f94ccf260acbc14f70170e37bcdac31a64c1420
TheLeoP <eugenio2305@hotmail.com>
2025-05-21 11:10:21 -0500

    ci: update actions

--- tests/powershell/powershell_spec.lua
+++ tests/powershell/powershell_spec.lua
@@ -3,9 +3,4 @@ local powershell = require "powershell"
 
 describe("setup", function()
   it("works with default", function() assert("my first function with param = Hello!", powershell.hello()) end)
-
-  it("works with custom var", function()
-    plugin.setup { opt = "custom" }
-    assert("my first function with param = custom", plugin.hello())
-  end)
 end)
